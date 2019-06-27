7567ee80a46ec7cf022d4d48da621c10d155a7a7
chathurangasandun <csk@tiqri.com>
2019-06-27 06:46:27 +0300

    travis and build process

---
+++ scripts/docker-publish.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+ls
+DOCKER_TAG='latest'
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+docker build -t accoon.pitstop.vehiclemanagement:$DOCKER_TAG src/Pitshop.CustomerApi/Accoon.Pitshop.VehicleApi.Presenter
+docker tag accoon.pitstop.vehiclemanagement:$DOCKER_TAG $DOCKER_USERNAME/accoon.pitstop.vehiclemanagement:$DOCKER_TAG
+docker push $DOCKER_USERNAME/accoon.pitstop.vehiclemanagement:$DOCKER_TAG
---
+++ scripts/dotnet-publish.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+dotnet publish ./src/Pitshop.VehicleApi/Accoon.Pitshop.VehicleApi.Presenter -c Release -o ./bin/docker
