d9e94e2973c2f22025424d03a3469c134c7bb088
chathurangasandun <csk@tiqri.com>
2019-06-27 07:05:51 +0300

    fix docker publish sh

--- scripts/docker-publish.sh
+++ scripts/docker-publish.sh
@@ -2,6 +2,6 @@
 ls
 DOCKER_TAG='latest'
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-docker build -t accoon.pitstop.vehiclemanagement:$DOCKER_TAG src/Pitshop.CustomerApi/Accoon.Pitshop.VehicleApi.Presenter
+docker build -t accoon.pitstop.vehiclemanagement:$DOCKER_TAG src/Pitshop.VehicleApi/Accoon.Pitshop.VehicleApi.Presenter
 docker tag accoon.pitstop.vehiclemanagement:$DOCKER_TAG $DOCKER_USERNAME/accoon.pitstop.vehiclemanagement:$DOCKER_TAG
 docker push $DOCKER_USERNAME/accoon.pitstop.vehiclemanagement:$DOCKER_TAG
